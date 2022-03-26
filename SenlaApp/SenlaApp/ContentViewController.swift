3c7dab74fec1b3871451d933c3133ea0728b84c6
igorpenkin <penkin.igor@gmail.com>
2022-03-26 16:48:51 +0300

    Adds window programatically

---
+++ SenlaApp/SenlaApp/ContentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ContentViewController.swift
+//  SenlaApp
+//
+//  Created by Igor Penkin on 26.03.2022.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray4
+        // Do any additional setup after loading the view.
+    }
+}
