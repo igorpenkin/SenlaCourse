87d0d1c08ecf55702f752de262f8f8e09640109f
igorpenkin <penkin.igor@gmail.com>
2022-03-26 16:48:51 +0300

    Adds window programatically

---
+++ SenlaApp/SenlaApp/InfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.swift
+//  SenlaApp
+//
+//  Created by Igor Penkin on 26.03.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
---
+++ SenlaApp/SenlaApp/NavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationController.swift
+//  SenlaApp
+//
+//  Created by Igor Penkin on 26.03.2022.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
